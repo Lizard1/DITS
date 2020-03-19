bf8ed0ce7842201e5fd53dba1689fd5a958de499
hp <sergey-zverev-1994@mail.ru>
2020-03-19 12:02:36 +0300

    NewParts

--- src/main/java/incubator/siteoftesting/model/Answer.java
+++ src/main/java/incubator/siteoftesting/model/Answer.java
@@ -21,12 +21,6 @@ public class Answer {
     @JoinColumn(name = "questionId")
     private Question questionA;
 
-    public int ifCorrect() {
-        if (correct != 0 || correct < 0) {
-            return 1;
-        } else return 0;
-    }
-
     public Answer() {
     }
 
